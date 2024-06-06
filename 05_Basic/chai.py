9452560e4143fea7a60698d2f9476e7426c20839
Ashish Singh <singhashish82103@gmail.com>
2024-06-06 14:19:25 +0530

    Intial commit

---
+++ 05_Basic/chai.py
@@ -0,0 +1,7 @@
+from hello_chai import ashish
+
+from hello_chai import extract
+
+ashish("ginger tea");
+
+extract(45);
---
+++ 05_Basic/hello_chai.py
@@ -0,0 +1,11 @@
+print("Ashish singh");
+
+def ashish (n):
+    print(n)
+
+ashish("lemon tea");
+
+def extract (a):
+    print(a)
+
+extract(43);
