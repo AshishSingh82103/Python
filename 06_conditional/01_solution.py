e353fc2eab020eab8afaa232eb4c4a6cb28377c6
Ashish Singh <singhashish82103@gmail.com>
2024-06-25 13:08:02 +0530

    Initial commit

--- 06_conditional/01_solution.py
+++ 06_conditional/01_solution.py
@@ -84,6 +84,16 @@ else:
 print("AI recommends you the transport of:",transport)
 
 # Q7:
+order_size = "Medium"
+extra_shot = True
+
+if extra_shot:
+    coffee = order_size + " coffee with an extra shot"
+else:
+    coffee = order_size + " coffee"
+print("Order:", coffee)
+
+# Q8:
 
 
 
