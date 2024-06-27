fd6374f8ca4bab037cb52ec7b7bdba8d8a047eb4
Ashish Singh <singhashish82103@gmail.com>
2024-06-27 15:22:16 +0530

    initial commit

--- 07_loops/08_solution.py
+++ 07_loops/08_solution.py
@@ -1,4 +1,14 @@
-number = n;
+number = 29
 
-for i in number:
-    
+is_prime = True
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            is_prime = False
+            break
+
+
+print(is_prime)
+            
+
