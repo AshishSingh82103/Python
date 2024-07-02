05b73ed2cd6721d651ebe644f8da30754d90ebf5
Ashish Singh <singhashish82103@gmail.com>
2024-07-02 08:49:02 +0530

    Initial commit

--- 07_loops/02_solution.py
+++ 07_loops/02_solution.py
@@ -6,3 +6,10 @@ for i in range(1, n + 1):
         sum_even_count += 1
 
 print("Sum of even number is:", sum_even_count)
+
+n = 20
+sum_event_number = 0;
+
+for i  in range(1, n + 1):
+    if i % 2 == 0:
+        sum_even_count += 1;
