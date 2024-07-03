3386e4743bbf15b98f1023ef0cd942d8a9d9c928
Ashish Singh <singhashish82103@gmail.com>
2024-07-03 12:16:49 +0530

    Initial commit

---
+++ 09_Functions/01_solution.py
@@ -0,0 +1,10 @@
+def square_of_num (number):
+    print(number ** 2)
+
+square_of_num(5)
+# -----------or----------
+def square_of_num (number):
+    return (number ** 2)
+
+result = square_of_num(5)
+print(result)
---
+++ 09_Functions/03_solution.py
@@ -0,0 +1,6 @@
+def multiplie (p1, p2):
+    return p1 * p2
+
+print(multiplie(8, 5))  # 40
+print(multiplie('a', 5)) # aaaaa
+print(multiplie(8, 'a')) # aaaaaaaa
