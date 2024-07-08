3d60ea6228f54a699019be3d3a8c50f9e15a4a3c
Ashish Singh <singhashish82103@gmail.com>
2024-07-08 10:22:16 +0530

    Initial commit

--- 11_Oops/01_solution.py
+++ 11_Oops/01_solution.py
@@ -1,3 +1,4 @@
+# 1
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
@@ -11,5 +12,27 @@ print(my_car.model)
 my_new_car = Car("Tata", "Nexon")
 print(my_new_car.brand)
 print(my_new_car.model)
+
+# 2
+class User:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+                
+    
+my_user = User("Api", "v834nf")
+print(my_user.brand)
+print(my_user.model)
+print(my_user.full_name())
+    
+       
+
+
+        
+
+
 
 
