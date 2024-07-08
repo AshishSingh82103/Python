b507b6250d43d87e226cc55d3055b65238ff5cae
Ashish Singh <singhashish82103@gmail.com>
2024-07-08 09:44:58 +0530

    Initial commit

--- 10_scopes/01_scope.py
+++ 10_scopes/01_scope.py
@@ -9,13 +9,49 @@ func()
 print(userName)
 
 
-x = 10 
-def func(y):
-    z = x + y 
-    return (x + y)
+# x = 10 
+# def func(y):
+#     z = x + y 
+#     return z
 
-result = func(1)
-print(result)
+# result = func(1)
+# print(result)
+
+# x = 99
+# def func3():
+#     global x
+#     x = 88
+# func3()
+# print(x) # 88
+
+# x = 99
+# def f1():
+#     # x = 88
+#     def f2():
+#         print(x)
+#     f2()
+# f1()
+
+x = 99
+def f1():
+    x = 88
+    def f2():
+        print(x)
+    return f2
+
+myResult = f1()
+myResult()
+
+def singhashish(num):
+    def actual(x):
+        return x ** num
+    return actual
+
+
+f = singhashish(2)
+g = singhashish(3)
+print(f(3))
+print(g(3))
 
 
 
