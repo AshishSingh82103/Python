bece18231951c2cec003b7bd0f6798153cb5f0ea
Ashish Singh <singhashish82103@gmail.com>
2024-07-09 11:08:32 +0530

    Initial commit

--- 11_Oops/01_solution.py
+++ 11_Oops/01_solution.py
@@ -69,17 +69,47 @@
 
 # 5. (Polymorphism)
 
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+    
+#     def full_name(self):
+#         return f"{self.brand} {self.model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Disel"
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+#     def fuel_type(self):
+#         return "Electirc charge"
+
+# my_tesla = ElectricCar("Tesla", "Model S", "85Kwh")
+# print(my_tesla.fuel_type())
+# my_tata = Car("Nexon", "5gtr",)
+# print(my_tata.fuel_type())
+
+
+# 6. Class Variables
+
 class Car:
+    total_car = 0
+
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
+        Car.total_car += 1
     
     def full_name(self):
         return f"{self.brand} {self.model}"
     
     def fuel_type(self):
         return "Petrol or Disel"
-    
+        
 class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
@@ -88,15 +118,12 @@ class ElectricCar(Car):
     def fuel_type(self):
         return "Electirc charge"
 
-
-
-
-my_tesla = ElectricCar("Tesla", "Model S", "85Kwh")
-print(my_tesla.fuel_type())
-my_tata = Car("Nexon", "5gtr",)
-print(my_tata.fuel_type())
-        
-
+safari = Car("Tata", "Safari")
+safari_two = Car("Tata", "Nexon")
+# print(safari.total_car)
+# test = Car("test", "test")
+# print(safari.total_car)
+print(Car.total_car)
 
 
 
