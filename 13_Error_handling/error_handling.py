f00b14c24abe4f471e5193bc90db0eabb5d226dc
Ashish Singh <singhashish82103@gmail.com>
2024-07-10 10:42:18 +0530

    Initial commit

---
+++ 13_Error_handling/error_handling.py
@@ -0,0 +1,13 @@
+# File handling
+
+file = open('youtube.txt', 'w')
+
+try:
+    file.write('chai ur code')
+finally:
+    file.close()
+
+#Other way and final way
+
+with open('youtbe.txt', 'w') as file:
+    file.write('chai ur code')
